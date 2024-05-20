c9b34c248b8fd1a25873685f71d71b191c8c91ad
SalvaPeris <sperisgimeno@gmail.com>
2024-05-20 13:03:47 +0200

    Added database context and repository interface.

---
+++ OData.WebApi/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OData.WebApi.Entities;
+
+namespace OData.WebApi.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContextOptions) : base(dbContextOptions)
+        { 
+        }
+
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ OData.WebApi/Persistence/Repositories/Interfaces/ICompanyRepository.cs
@@ -0,0 +1,13 @@
+﻿using OData.WebApi.Entities;
+
+namespace OData.WebApi.Persistence.Repositories.Interfaces
+{
+    public interface ICompanyRepository
+    {
+        public IQueryable<Company> GetAll();
+        public IQueryable<Company> GetById(int id);
+        public void Create(Company company);
+        public void Update(Company company);
+        public void Delete(Company company);
+    }
+}
