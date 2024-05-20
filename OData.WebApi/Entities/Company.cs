666b38cd75ae75c6066f651ab5a3eadb9d5f43c5
SalvaPeris <sperisgimeno@gmail.com>
2024-05-20 12:58:10 +0200

    Agregar archivos de proyecto.

---
+++ OData.WebApi/Entities/Company.cs
@@ -0,0 +1,10 @@
+﻿namespace OData.WebApi.Entities
+{
+    public class Company
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public int Size { get; set; }
+        public List<Product>? Products { get; set; }
+    }
+}
---
+++ OData.WebApi/Entities/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace OData.WebApi.Entities
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public Guid CompanyId { get; set; }
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
